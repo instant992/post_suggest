7ecc5f6fd0c3ce1c814e975761bc67ff5c819968
ProstoLive <artem009saratov@gmail.com>
2024-03-07 21:36:05 +0400

    Code restructuring. Anonymous messages added.

---
+++ actions/actions.py
@@ -0,0 +1,86 @@
+from telebot import types
+from config import TARGET_CHANNEL_ID, ADMIN_CHAT_ID, BOT_USERNAME
+from string import Template
+from config import POST_SAMPLE
+
+post_template = Template(POST_SAMPLE)
+
+def send_approved_message(bot, message, author_chat_id, message_id_in_author_chat):
+    if message.text:
+        bot.send_message(TARGET_CHANNEL_ID, message.text)
+    elif message.photo:
+        bot.send_photo(TARGET_CHANNEL_ID, message.photo[-1].file_id, caption=message.caption)
+    elif message.video:
+        bot.send_video(TARGET_CHANNEL_ID, message.video_file_id, caption=message.caption)
+    else:
+        bot.send_message(ADMIN_CHAT_ID, "Error: Message object is missing.")
+
+    bot.send_message(author_chat_id, "🔥🔥Ваш пост был одобрен и отправлен в канал! ", reply_to_message_id= message_id_in_author_chat)
+
+def send_declined_message(bot, message, author_chat_id, message_id_in_author_chat):
+    if message:
+        bot.send_message(author_chat_id, "❌Ваш пост был отклонён.", reply_to_message_id=message_id_in_author_chat)
+    else:
+        bot.send_message(ADMIN_CHAT_ID, "Error: Message object is missing.")
+
+def approved_from_user_message(bot, message, message_id_in_author_chat):
+    confirmation_markup = types.InlineKeyboardMarkup()
+    approve_button = types.InlineKeyboardButton("✅Подтвердить", callback_data=f"admin_post_approve:{message.chat.id}:{message_id_in_author_chat}")
+    decline_button = types.InlineKeyboardButton("❌Отклонить", callback_data=f"admin_post_decline:{message.chat.id}:{message_id_in_author_chat}")
+    confirmation_markup.add(approve_button, decline_button)
+
+    if message.text:
+        text = f"{message.text}\n\n" + post_template.substitute(post_author=message.chat.username,
+                                                                bot_username=BOT_USERNAME)
+        bot.send_message(ADMIN_CHAT_ID, text, reply_markup=confirmation_markup)
+        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        print(f"Text message {message.text} from {message.chat.username} sent to admin-panel")
+    elif message.photo:
+        photo_file_id = message.photo[-1].file_id
+        message.caption = message.caption if message.caption else ''
+        caption = f"{message.caption}\n\n" + post_template.substitute(post_author = message.chat.username, bot_username = BOT_USERNAME)
+        bot.send_photo(ADMIN_CHAT_ID, photo_file_id, caption=caption, reply_markup=confirmation_markup)
+        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        print(f"Photo-message with caption {message.caption} from {message.chat.username} ")
+    elif message.video:
+        video_file_id = message.video.file_id
+        message.caption = message.caption if message.caption else ''
+        caption = f"{message.caption}\n\n" + post_template.substitute(post_author=message.chat.username,
+                                                                      bot_username=BOT_USERNAME)
+        bot.send_video(ADMIN_CHAT_ID, video_file_id, caption=caption, reply_markup=confirmation_markup)
+        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        print(f"Video-message with caption {message.caption} from {message.chat.username} ")
+
+
+def anonymous_from_user_message(bot, message, message_id_in_author_chat):
+    confirmation_markup = types.InlineKeyboardMarkup()
+    approve_button = types.InlineKeyboardButton("✅Подтвердить", callback_data=f"admin_post_approve:{message.chat.id}:{message_id_in_author_chat}")
+    decline_button = types.InlineKeyboardButton("❌Отклонить", callback_data=f"admin_post_decline:{message.chat.id}:{message_id_in_author_chat}")
+    confirmation_markup.add(approve_button, decline_button)
+
+    if message.text:
+        text = f"{message.text}\n\n" + post_template.substitute(post_author="Аноним",
+                                                                bot_username=BOT_USERNAME)
+        bot.send_message(ADMIN_CHAT_ID, text, reply_markup=confirmation_markup)
+        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        print(f"Text message {message.text} from {message.from_user.username} sent to admin-panel")
+    elif message.photo:
+        photo_file_id = message.photo[-1].file_id
+        message.caption = message.caption if message.caption else ''
+        caption = f"{message.caption}\n\n" + post_template.substitute(post_author="Аноним",
+                                                                      bot_username=BOT_USERNAME)
+        bot.send_photo(ADMIN_CHAT_ID, photo_file_id, caption=caption, reply_markup=confirmation_markup)
+        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        print(f"Photo-message with caption {message.caption} ")
+    elif message.video:
+        video_file_id = message.video.file_id
+        message.caption = message.caption if message.caption else ''
+        caption = f"{message.caption}\n\n" + post_template.substitute(post_author="Аноним",
+                                                                      bot_username=BOT_USERNAME)
+        bot.send_video(ADMIN_CHAT_ID, video_file_id, caption=caption, reply_markup=confirmation_markup)
+        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+
+
+def declined_from_user_message(bot, author_chat_id):
+    bot.send_message(author_chat_id, "Сообщение отменено. Жду новый вариант!")
+
--- main.py
+++ main.py
@@ -1,73 +1,65 @@
 import telebot
 from telebot import types
-from string import Template
-from config import BOT_TOKEN, ADMIN_CHAT_ID, POST_SAMPLE, BOT_USERNAME, TARGET_CHANNEL_ID
+
+from config import BOT_TOKEN, ADMIN_CHAT_ID
+
+from actions.actions import send_approved_message,send_declined_message, approved_from_user_message, declined_from_user_message, anonymous_from_user_message
 
 bot = telebot.TeleBot(BOT_TOKEN)
-
-post_template = Template(POST_SAMPLE)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Приветствую! Пришлите фото или видео с сообщением или без.")
+    print(f"Registered start message from user {message.from_user.username} in chat {message.chat.id}")
 
 @bot.message_handler(content_types=["photo", "text", "video"])
 def handle_message(message):
     # Copy message content and send to admin with inline keyboard
-    markup = types.InlineKeyboardMarkup()
-    approve_button = types.InlineKeyboardButton("✅Запостить", callback_data=f"approve:{message.message_id}:{message.chat.id}")
-    decline_button = types.InlineKeyboardButton("❌Отклонить", callback_data=f"decline:{message.message_id}:{message.chat.id}")
-    markup.add(approve_button, decline_button)
+    confirmation_markup = types.InlineKeyboardMarkup()
+    approve_button = types.InlineKeyboardButton("✅Подтвердить", callback_data=f"user_post_approve:{message.chat.id}:{message.id}")
+    decline_button = types.InlineKeyboardButton("❌Отклонить", callback_data=f"user_post_decline:{message.chat.id}:{message.id}")
+    anonymous_button = types.InlineKeyboardButton("🙈Отправить анонимно", callback_data=f"user_post_anonymous:{message.chat.id}:{message.id}")
+    confirmation_markup.add(approve_button, decline_button)
+    confirmation_markup.add(anonymous_button)
 
     if message.text:
-        text = f"{message.text}\n\n" + post_template.substitute(post_author=message.from_user.username, bot_username = BOT_USERNAME)
-        bot.send_message(ADMIN_CHAT_ID, text, reply_markup=markup)
-        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        bot.send_message(message.chat.id, message.text, reply_markup=confirmation_markup)
     elif message.photo:
         photo_file_id = message.photo[-1].file_id
         message.caption = message.caption if message.caption else ''
-        caption = f"{message.caption}\n\n" + post_template.substitute(post_author = message.from_user.username, bot_username = BOT_USERNAME)
-        bot.send_photo(ADMIN_CHAT_ID, photo_file_id, caption=caption, reply_markup=markup)
-        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        bot.send_photo(message.chat.id, photo_file_id, caption=message.caption, reply_markup=confirmation_markup)
     elif message.video:
         video_file_id = message.video.file_id
         message.caption = message.caption if message.caption else ''
-        caption = f"{message.caption}\n\n" + post_template.substitute(post_author=message.from_user.username, bot_username=BOT_USERNAME)
-        bot.send_video(ADMIN_CHAT_ID, video_file_id, caption=caption, reply_markup=markup)
-        bot.send_message(message.chat.id, "Ваш пост успешно отправлен на рассмотрение!👍")
+        bot.send_video(message.chat.id, video_file_id, caption=message.caption, reply_markup=confirmation_markup)
 
+#For admin approvement
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
     # Extract message ID and action from callback data
-    action, message_id, author_chat_id = call.data.split(":")
+    action, author_chat_id, message_id_in_author_chat = call.data.split(":")
     message = call.message  # Extract the message object
 
-    if action == "approve":
-        # Check if the message object exists
-        if message.text:
-            # Send approved message to channel
-            bot.send_message(TARGET_CHANNEL_ID, message.text)
-        elif message.photo:
-            bot.send_photo(TARGET_CHANNEL_ID, message.photo[-1].file_id, caption=message.caption)
-        elif message.video:
-            bot.send_video(ADMIN_CHAT_ID, message.video_file_id, caption=message.caption)
-        else:
-            bot.send_message(ADMIN_CHAT_ID, "Error: Message object is missing.")
-        bot.edit_message_reply_markup(ADMIN_CHAT_ID, call.message.id, reply_markup=None)
-        bot.send_message(author_chat_id, "🔥🔥Ваш пост был одобрен и отправлен в канал! ", reply_to_message_id=message_id)
+    match action:
+        case "user_post_approve":
+            approved_from_user_message(bot, message, message_id_in_author_chat)
+            bot.edit_message_reply_markup(author_chat_id, call.message.id, reply_markup=None)
+        case "user_post_decline":
+            declined_from_user_message(bot, author_chat_id)
+            bot.edit_message_reply_markup(author_chat_id, call.message.id, reply_markup=None)
+        case "user_post_anonymous":
+            anonymous_from_user_message(bot, message, message_id_in_author_chat)
+            bot.edit_message_reply_markup(author_chat_id, call.message.id, reply_markup=None)
+        case "admin_post_approve":
+            send_approved_message(bot, message, author_chat_id, message_id_in_author_chat)
+            bot.edit_message_reply_markup(ADMIN_CHAT_ID, call.message.id, reply_markup=None)
+        case "admin_post_decline":
+            send_declined_message(bot, message, author_chat_id, message_id_in_author_chat)
+            bot.edit_message_reply_markup(ADMIN_CHAT_ID, call.message.id, reply_markup=None)
 
-    elif action == "decline":
-        # Check if the message object exists
-        if message:
-            # Notify user
-            bot.send_message(author_chat_id, "❌Ваш пост был отклонён.", reply_to_message_id=message_id)
-        else:
-            bot.send_message(ADMIN_CHAT_ID, "Error: Message object is missing.")
-
-        bot.edit_message_reply_markup(ADMIN_CHAT_ID, call.message.id, reply_markup=None)
 
 # Enable bot to receive callback queries
 bot.enable_save_next_step_handlers(delay=2)
 bot.load_next_step_handlers()
 
-bot.polling()
+bot.infinity_polling(timeout=10, long_polling_timeout = 5)
